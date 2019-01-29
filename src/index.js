cb9dbfebae542acbe908a82c1a00f19ec18d45b4
xXDuckXx <dacs2010@gmail.com>
2019-01-29 18:02:42 -0500

    learning still

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
 
 function formatUser(user){
-    return user.firstName + ' ' +  user.lastName
+    return user.firstName + ' ' +  user.lastName;
 }
 
 
 const user = {
-    firstName: 'David',
-    lastName: 'Sedgwick'
-}
+    firstName: '∂ß',
+    lastName: ''
+};
 
 const element = (
-    <h1>
-        Hello {formatUser(user)}!
-    </h1>
-)
+   <div> tabIndex='0'</div>
+);
 
 
-
-
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello,{formatUser(user)}!</h1>
+    }
+    return <h1>Hello, Stranger.</h1>
+}
 
 
 ReactDOM.render(
     element,
     document.querySelector('#root')
-)
+);
