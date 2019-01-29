59cccf59fcd8834786955476582346bd01238de9
xXDuckXx <dacs2010@gmail.com>
2019-01-29 17:23:59 -0500

    prints my name

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function formatUser(user){
+    return user.firstName + ' ' +  user.lastName
+}
+
+
+const user = {
+    firstName: 'David',
+    lastName: 'Sedgwick'
+}
+
+const element = (
+    <h1>
+        Hello {formatUser(user)}!
+    </h1>
+)
+
+
+
+
+
+
+ReactDOM.render(
+    element,
+    document.querySelector('#root')
+)
